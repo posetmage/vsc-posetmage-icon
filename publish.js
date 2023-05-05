621edd1de3e7cc9847d323eabfcf6d871cb42306
PosetMage <posetmage@gmail.com>
2023-05-05 12:04:46 +0800

    v1.0.25: add to commit

--- publish.js
+++ publish.js
@@ -1,20 +1,33 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
+const program = require('commander');
 const standardVersion = require('standard-version');
 
+program
+  .option('-m, --message <path>', 'Path to commit message file') // Add the option before parse()
+  .parse(process.argv);
+
 async function main() {
-  const packageJsonPath = 'package.json';
-  const tokenPath = 'C:\\PosetMage\\Package\\_token\\azure_token';
+  const commitMessagePath = program.opts().message;
   const changelogPath = 'CHANGELOG.md';
+  const defaultCommitMessage = 'Update';
+  const commitMessage = commitMessagePath ? fs.readFileSync(commitMessagePath, 'utf8').trim() : defaultCommitMessage;
+
+  if (!commitMessagePath) {
+    console.error('Error: No commit message path provided.');
+    return;
+  }
+
+
+
+  // Set git user name and email
   execSync(`git config --local user.name "PosetMage"`);
   execSync(`git config --local user.email "posetmage@gmail.com"`);
 
   // Get the current version
+  const packageJsonPath = 'package.json';
   const packageData = fs.readFileSync(packageJsonPath, 'utf8');
   const { version: currentVersion } = JSON.parse(packageData);
-
-  const commitMessagePath = process.argv[3] === '--changelog' ? process.argv[4] : null;
-  const commitMessage = commitMessagePath ? fs.readFileSync(commitMessagePath, 'utf8').trim() : '';
 
   // Run git commands
   execSync(`git add .`);
@@ -35,6 +48,7 @@ async function main() {
   execSync(`vsce package`);
 
   // Publish the extension
+  const tokenPath = 'C:\\PosetMage\\Package\\_token\\azure_token';
   const token = fs.readFileSync(tokenPath, 'utf8').trim();
   execSync(`vsce publish --pat ${token}`);
 }
