8417947d353be9ffa63930b02aa4c65e227987c5
PosetMage <posetmage@gmail.com>
2023-05-05 10:33:47 +0800

    Update to version 1.0.12

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is just posetmage icon showing
 
-Version: 1.0.11
+Version: 1.0.12
 
 ## marketplace
 https://marketplace.visualstudio.com/items?itemName=PosetMage.posetmage-icon
--- src/extension.ts
+++ src/extension.ts
@@ -2,28 +2,21 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-import { MyCustomView } from './myCustomView';
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const myCustomView = new MyCustomView();
-	context.subscriptions.push(
-		vscode.window.registerTreeDataProvider('myCustomView', myCustomView)
-	);
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "show-icon" is now active!');
+	console.log('Congratulations, your extension "posetmage-icon" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('show-icon.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('posetmage-icon.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Show Icon!');
+		vscode.window.showInformationMessage('Hello World from PosetMage Icon!');
 	});
 
 	context.subscriptions.push(disposable);
